a151f8f48f14955f18c21540b09d161337c3a619
jimmymaise <the@duyet.com>
2023-05-17 10:48:15 -0700

    add call api to get list traders

--- src/debug.py
+++ src/debug.py
@@ -1,27 +1,73 @@
 import json
-
+import click
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from twisted.internet import reactor
-
+from utils.constant import Constant
 from scrape_signals.spiders.zulu_trade_api import ZuluTradeSpiderAPI
+import requests
 
 configure_logging(install_root_handler=True)
+count = 1
 
 
-def run_crawl():
-    """
-    Run a spider within Twisted. Once it completes,
-    schedule the next spider to run immediately.
-    """
-    with open('./trader_ids.json') as content:
-        trader_ids = json.load(content)
-    runner = CrawlerRunner(get_project_settings())
-    deferred = runner.crawl(ZuluTradeSpiderAPI, external_trader_ids=trader_ids['trader_ids'])
-    deferred.addCallback(lambda _: reactor.callLater(1, run_crawl))
-    return deferred
+class CrawlHandler:
+    def __init__(self, runner_name) -> None:
+        self.runner_name = runner_name
+
+    def get_trader_ids(self):
+        url = Constant.CREATE_RUNNER_IF_NOT_EXIST_URL
+        headers = {"Content-Type": "application/json"}
+        data = {"name": self.runner_name, "crawler_type": Constant.ZULU_API_SPIDER_NAME}
+        try:
+            resp = requests.post(url, headers=headers, json=data)
+            if resp.status_code in [requests.codes.created, requests.codes.ok]:
+                assignments = resp["assignments"]
+                if assignments:
+                    print(assignments)
+
+            raise Exception(
+                f"Invalid status status {resp.status_code} and {resp.content}"
+            )
+
+        except Exception as e:
+            self.logger.error(e)
+
+    def run_crawl(self):
+        """
+        Run a spider within Twisted. Once it completes,
+        schedule the next spider to run immediately.
+        """
+
+        import os
+
+        print(os.getcwd())
+        with open("./trader_ids.json") as content:
+            trader_ids = json.load(content)
+        runner = CrawlerRunner(get_project_settings())
+
+        ab = self.get_trader_ids()
+        global count
+        count += 1
+        print(f"AAAAAAAAAAAAAAAAAAAAA-AAAAAAAAAAAAAAAAAAAAA{count}")
+        deferred = runner.crawl(
+            ZuluTradeSpiderAPI, external_trader_ids=trader_ids["trader_ids"]
+        )
+        deferred.addCallback(lambda _: reactor.callLater(1, self.run_crawl))
+        return deferred
+
+    def start(self):
+        self.run_crawl()
+        reactor.run()
 
 
-run_crawl()
-reactor.run()
+@click.command()
+@click.option("--runner-id", default=1, help="Name Runner.")
+def start_runner(runner_id):
+    """Simple program that greets NAME for a total of COUNT times."""
+    CrawlHandler(runner_id).start()
+
+
+if __name__ == "__main__":
+    start_runner()
--- src/utils/constant.py
+++ src/utils/constant.py
@@ -1,8 +1,16 @@
-class Constant:
-    ZULU_API_URL_TEMPLATE = 'https://www.zulutrade.com/zulutrade-client/trading/api/providers/{external_trader_id}/trades/open/all'
-    ZULU_API_SPIDER_NAME = 'zulu_api'
-    ZULU_API_ALLOWED_DOMAINS = ['zulutrade.com']
-    ZULU_API_SOURCE_NAME = 'zulu'
+import os
 
-    CONTROLLER_BASE_URL = 'http://127.0.0.1:8000'
-    MASTER_TRADER_UPSERT_TRADER_CONTROLLER_URL = CONTROLLER_BASE_URL + '/master_traders/'
+
+class Constant:
+    ZULU_API_URL_TEMPLATE = "https://www.zulutrade.com/zulutrade-client/trading/api/providers/{external_trader_id}/trades/open/all"
+    ZULU_API_SPIDER_NAME = "zulu_api"
+    ZULU_API_ALLOWED_DOMAINS = ["zulutrade.com"]
+    ZULU_API_SOURCE_NAME = "zulu"
+
+    CONTROLLER_BASE_URL = os.getenv("CONTROLLER_BASE_URL", "http://127.0.0.1:8000")
+    MASTER_TRADER_UPSERT_TRADER_CONTROLLER_URL = (
+        CONTROLLER_BASE_URL + "/master_traders/"
+    )
+    CREATE_RUNNER_IF_NOT_EXIST_URL = (
+        CONTROLLER_BASE_URL + "/crawl_runners/"
+    )
